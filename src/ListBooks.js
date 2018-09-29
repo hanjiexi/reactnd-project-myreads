51fc00ba45ed90c9cf232781fd7ad688c06b91cf
Josh Hanson <hanjiexi@gmail.com>
2018-09-29 16:00:45 -0400

    feat: implement React-Router

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Shelf from './Shelf';
+import { Link } from 'react-router-dom';
 
 class ListBooks extends React.Component {
   render() {
@@ -12,7 +13,7 @@ class ListBooks extends React.Component {
           <Shelf />
         </div>
         <div className="open-search">
-          <a onClick={() => this.props.showSearch()}>Add a book</a>
+          <Link to='/search'>Add a book</Link>
         </div>
       </div>
     );
