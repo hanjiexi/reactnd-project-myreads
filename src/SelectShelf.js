4ed0e58df419db5bac1b160b726b1e1cb257ea6a
Josh Hanson <hanjiexi@gmail.com>
2018-09-29 23:11:03 -0400

    fix: defaultValue in select

--- src/SelectShelf.js
+++ src/SelectShelf.js
@@ -4,14 +4,14 @@ import Shelves from './Shelves';
 function SelectShelf(props) {
   return (
     <div className="book-shelf-changer">
-      <select onChange={e => props.move(props.book, e.target.value)}>
+      <select
+        defaultValue={props.book.shelf}
+        onChange={e => props.move(props.book, e.target.value)}
+      >
         <option value="move" disabled>Move to...</option>
         {Shelves.t.map(s => (
-          props.shelf === s
-          ? <option selected key={s} value={s}>{Shelves.idToName(s)}</option>
-          : <option key={s} value={s}>{Shelves.idToName(s)}</option>
+          <option key={props.book.id} value={s}>{Shelves.idToName(s)}</option>
         ))}
-        <option value="none">None</option>
       </select>
     </div>
   );
